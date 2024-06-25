9a6f1819cec798385ab90cc61f57fc6b974ead9c
ixingchen <152966163+IIXINGCHEN@users.noreply.github.com>
2024-06-25 14:19:39 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.lixd.xyz": dockerHub,
-  "quay.lixd.xyz": "https://quay.io",
-  "gcr.lixd.xyz": "https://gcr.io",
-  "k8s-gcr.lixd.xyz": "https://k8s.gcr.io",
-  "k8s.lixd.xyz": "https://registry.k8s.io",
-  "ghcr.lixd.xyz": "https://ghcr.io",
-  "cloudsmith.lixd.xyz": "https://docker.cloudsmith.io",
+  "docker.imixc.top": dockerHub,
+  "quay.imixc.top": "https://quay.io",
+  "gcr.imixc.top": "https://gcr.io",
+  "k8s-gcr.imixc.top": "https://k8s.gcr.io",
+  "k8s.imixc.top": "https://registry.k8s.io",
+  "ghcr.imixc.top": "https://ghcr.io",
+  "cloudsmith.imixc.top": "https://docker.cloudsmith.io",
 
   // staging
   "docker-staging.lixd.xyz": dockerHub,
